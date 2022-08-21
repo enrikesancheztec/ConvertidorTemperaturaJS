7c989cb196b55873857d830687816b6e7280f029
Enrique Sanchez <enrike.sanchez@gmail.com>
2022-08-21 17:03:56 -0600

    Fix Port

--- app.js
+++ app.js
@@ -23,4 +23,5 @@ app.use(cors());
 app.use('/', apiRoutes);
 
 // Starting server
-app.listen('8080')
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
